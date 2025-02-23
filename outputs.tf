fbc949fb0eddb8b30dcad744eba0c86d77f76e98
Dan <danffreitas@gmail.com>
2025-02-23 16:08:28 -0300

    feat: adding s3 bucket

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "IPs públicos"
   value       = aws_instance.this.public_ip
 }
+
+output "bucket_data" {
+  description = "Url do bucket"
+  value       = aws_s3_bucket.this.bucket_domain_name
+}
